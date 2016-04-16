414172cc432004f29ef6181f115cc5189265860f
salchi <d.salchner@tsn.at>
2016-04-16 16:14:53 +0200

    rotation commit 2

---
+++ LegoPacman/LegoPacman/Program.cs
@@ -0,0 +1,51 @@
+﻿using LegoPacman.classes;
+using MonoBrickFirmware.Movement;
+using MonoBrickFirmware.UserInput;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LegoPacman
+{
+    class Program
+    {
+        private static Roboter roboter;
+
+        static void Main(string[] args)
+        {
+            var terminateProgram = new ManualResetEvent(false);
+            var buttonEvents = new ButtonEvents();
+
+            buttonEvents.EscapePressed += () =>
+            {
+                terminateProgram.Set();
+            };
+
+            roboter = new Roboter();
+            TestRotation(90);
+            TestRotation(270);
+            TestRotation(180);
+            TestRotation(360);
+            TestRotation(540);
+            TestRotation(810);
+            TestRotation(-90);
+            TestRotation(-180);
+            TestRotation(-270);
+            TestRotation(-360);
+            TestRotation(-810);
+            TestRotation(-900);
+
+            terminateProgram.WaitOne();
+        }
+
+        private static void TestRotation(int degrees)
+        {
+            MonoBrickFirmware.Display.LcdConsole.WriteLine("{0} degrees", degrees);
+            roboter.Rotate(degrees);
+            Thread.Sleep(1000);
+        }
+    }
+}
---
+++ LegoPacman/LegoPacman/classes/Roboter.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MonoBrickFirmware.Sensors;
+using MonoBrickFirmware.Movement;
+using MonoBrickFirmware.Display;
+
+namespace LegoPacman.classes
+{
+    enum RotationDirection
+    {
+        left,right
+    }
+
+    class Roboter
+    {
+        private const SensorPort PORT_GYRO = SensorPort.In2;
+        private const MotorPort PORT_MOTOR_LEFT = MotorPort.OutA;
+        private const MotorPort PORT_MOTOR_RIGHT = MotorPort.OutD;
+
+        private const int BOUND_REDUCE_SPEED = 5;
+
+        private EV3GyroSensor gyroSensor;
+        private Vehicle vehicle;
+
+        public Roboter()
+        {
+            gyroSensor = new EV3GyroSensor(PORT_GYRO, GyroMode.Angle);
+            gyroSensor.Reset();
+            vehicle = new Vehicle(PORT_MOTOR_LEFT, PORT_MOTOR_RIGHT);
+        }
+
+        public void Rotate(int degrees)
+        {
+            degrees %= 360;
+            degrees = (degrees < 0) ? degrees + 360 : degrees;
+
+            LcdConsole.WriteLine("degrees: {0}", degrees);
+
+            var currentAngle = gyroSensor.Read();
+            var targetAngle = GetTargetAngle(currentAngle, degrees);
+            var delta = targetAngle - currentAngle;
+
+            if (delta <= 180 && delta > 0)
+            {
+                LcdConsole.WriteLine("rotating left... delta {0}", delta);
+                DoRotate(targetAngle, RotationDirection.left);
+            }
+            else if(delta < 0 && delta >= -180)
+            {
+                LcdConsole.WriteLine("rotating right... delta {0}", delta);
+                DoRotate(targetAngle, RotationDirection.right);
+            }
+        }
+
+        private int GetTargetAngle(int startAngle, int degreesToRotate)
+        {
+            return (startAngle + degreesToRotate) % 360;
+        }
+
+        private sbyte GetRotatingSpeed(int delta)
+        {
+            return Convert.ToSByte((delta > BOUND_REDUCE_SPEED) ? 100 : 10);
+        }
+
+        private void DoRotate(int targetAngle, RotationDirection direction)
+        {
+            var currentAngle = gyroSensor.Read();
+            var speed = GetRotatingSpeed(Math.Abs(targetAngle - currentAngle));
+
+            LcdConsole.WriteLine("currentAngle: {0}; speed:{1}",currentAngle, speed);
+            switch (direction)
+            {
+                case RotationDirection.left:
+                    vehicle.SpinLeft(speed);
+                    break;
+                case RotationDirection.right:
+                    vehicle.SpinRight(speed);
+                    break;
+            }
+
+            while (currentAngle != targetAngle)
+            {
+                currentAngle = gyroSensor.Read();
+            }
+
+            vehicle.Brake();
+        }
+    }
+}
